fddb9bec477d8a0db29439a7c01bc63f5db5373a
martafronton <martafronton@gmail.com>
2024-11-28 12:19:42 +0100

    radiocirculo

---
+++ Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Para calcular el área y la circunferencia del círculo introduzca la longuitud del radio")
+    var radio = readln().toInt()
+    var area = 3.14 * radio //pi lo representamos como 3,14 a no ser que nos digan lo contrario
+    var circunferencia =  2 * radio * 3.14
+    println("El área del círculo es igual a $area y la circunferencia es $circunferencia")
+}
